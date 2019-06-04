8b2d7235af94259fc23dbd73e1c0710fa6188b1b
tr276913271 <276913271@qq.com>
2019-06-04 18:29:34 +0800

    Update RabbitConsumer.java

--- study-rabbitmq/src/main/java/me/kagami/mq/RabbitConsumer.java
+++ study-rabbitmq/src/main/java/me/kagami/mq/RabbitConsumer.java
@@ -1,6 +1,10 @@
 package me.kagami.mq;
 
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class RabbitConsumer {
     private static final String EXCHANGE_NAME = "exchange_demo";
@@ -8,9 +12,35 @@ public class RabbitConsumer {
     private static final String QUEUE_NAME = "queue_demo";
     private static final String IP_ADDRESS = "127.0.0.1";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        Address[] addresses = new Address[]{
+                new Address(IP_ADDRESS, 5672)
+        };
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUsername("guest");
         factory.setPassword("guest");
+        Connection connection = factory.newConnection(addresses);
+        final Channel channel = connection.createChannel();
+        channel.basicQos(64);//客户端最多未被接收ack的消息个数
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("recv message" + new String(body));
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        };
+
+        channel.basicConsume(QUEUE_NAME, consumer);
+
+        TimeUnit.SECONDS.sleep(5);
+        channel.close();
+        connection.close();
     }
 }
